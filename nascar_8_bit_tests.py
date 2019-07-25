0c15d645d2a9c73ce656a870d279cd4965203208
Jose Chavez <jose.chavez@slalom.com>
2019-07-25 01:40:16 -0500

    Refactored unit test to use setup

--- nascar_8_bit_tests.py
+++ nascar_8_bit_tests.py
@@ -3,56 +3,41 @@ from rewardfunctions import nascar_8_bit
 
 
 class RewardFunctionTests(unittest.TestCase):
+    def setUp(self):
+        self.params = {
+            "all_wheels_on_track": True,
+            "speed": 3,
+            "waypoints": [[5, 0], [0, 0]],
+            "closest_waypoints": [0, 1],
+            "heading": 180,
+        }
+
     def test_speed_on_track_increasing_reward(self):
         for speed in range(3, 5):
             with self.subTest(speed=speed):
-                params = {
-                    "all_wheels_on_track": True,
-                    "speed": speed,
-                    "waypoints": [[5, 0], [0, 0]],
-                    "closest_waypoints": [0, 1],
-                    "heading": 180,
-                }
-                reward = nascar_8_bit(params)
+                self.params["speed"] = speed
+                reward = nascar_8_bit(self.params)
                 self.assertGreaterEqual(reward, 20, "speed=%d" % speed)
 
     def test_speed_on_track_decreasing_reward(self):
         for speed in [1, 2]:
             with self.subTest(speed=speed):
-                params = {
-                    "all_wheels_on_track": True,
-                    "speed": speed,
-                    "waypoints": [[5, 0], [0, 0]],
-                    "closest_waypoints": [0, 1],
-                    "heading": 180,
-                }
-                reward = nascar_8_bit(params)
+                self.params["speed"] = speed
+                reward = nascar_8_bit(self.params)
                 self.assertLessEqual(reward, 10, "speed=%d" % speed)
 
     def test_heading_on_track_increasing_reward(self):
         for heading in [170, 180, 190]:
             with self.subTest(heading=heading):
-                params = {
-                    "all_wheels_on_track": True,
-                    "speed": 3,
-                    "waypoints": [[5, 0], [0, 0]],
-                    "closest_waypoints": [0, 1],
-                    "heading": heading,
-                }
-                reward = nascar_8_bit(params)
+                self.params["heading"] = heading
+                reward = nascar_8_bit(self.params)
                 self.assertGreaterEqual(reward, 20, "heading=%d" % heading)
 
     def test_heading_on_track_decreasing_reward(self):
         for heading in [160, 200]:
             with self.subTest(heading=heading):
-                params = {
-                    "all_wheels_on_track": True,
-                    "speed": 3,
-                    "waypoints": [[5, 0], [0, 0]],
-                    "closest_waypoints": [0, 1],
-                    "heading": heading,
-                }
-                reward = nascar_8_bit(params)
+                self.params["heading"] = heading
+                reward = nascar_8_bit(self.params)
                 self.assertLessEqual(reward, 10, "heading=%d" % heading)
 
 
